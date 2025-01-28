f3c12d67406a80a7d2cc40183668cdacf68ea1d1
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2025-01-28 11:17:05 -0500

    fix json

--- OpenGarage/main.cpp
+++ OpenGarage/main.cpp
@@ -360,9 +360,9 @@ void sta_logs_fill_json(String& json, OTF::Response &res) {
 	json = "";
 	json += F("{\"name\":\"");
 	json += og.options[OPTION_NAME].sval;
-    json += F(",\"fwv\":");
+    json += F("\",\"fwv\":");
 	json += og.options[OPTION_FWV].ival;
-	json += F("\",\"starttime\":");
+	json += F(",\"starttime\":");
 	json += start_utc_time;
 	json += F(",\"time\":");
 	json += curr_utc_time;
