f9ad9af19f74c2386dfec971a1f1c1e78347fdff
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2025-01-28 16:16:48 -0500

    generate gziped files

---
+++ OpenGarage/web/build.mjs
@@ -0,0 +1,27 @@
+import { gzip } from 'pako'
+import { glob } from 'glob'
+import { readFileSync, createWriteStream } from 'fs';
+
+const files = await glob('public/*.{html,css,js}');
+const output = createWriteStream("../htmls.h")
+
+output.write("#ifndef HTML_H\n#define HTML_H\n")
+files.forEach((path) => {
+    const content = readFileSync(path);
+
+    
+    const name = path.replace("public/", "").replace(".", "_");
+    let compressed = gzip(content);
+    output.write(`const size_t ${name}_size = ${compressed.length};\n static const char ${name}[] PROGMEM = {`)
+    compressed.forEach((byte, idx) => {
+        if (idx > 0) {
+            output.write(`, `);
+        }
+
+        output.write(`0x${byte.toString(16).padStart(2, '0')}`);
+    });
+    output.write("};\n");
+
+});
+output.write("#endif\n")
+output.end();
