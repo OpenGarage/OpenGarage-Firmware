56201cec2a7bdff25d59ed0ecb741737c2c2bfda
Ray <rayshobbyshop@gmail.com>
2025-10-06 14:35:50 -0400

    Deployed 7420e49 with MkDocs version: 1.5.3

--- assets/bins/flash.sh
+++ assets/bins/flash.sh
@@ -1,3 +1,3 @@
-~/esp8266_3.0/tools/esptool/esptool.py -p /dev/ttyUSB? -b 460800 -a soft_reset write_flash 0x00000 og_1.2.3.bin
+~/esp8266_3.0/tools/esptool/esptool.py -p /dev/ttyUSB? -b 460800 -a soft_reset write_flash 0x00000 og_1.2.4.bin
 sleep 3
 gtkterm -p /dev/ttyUSB0 -s 115200
